bbfa49edb2cec86ed912a3142483494bc1f0311f
Michał Matysiak <michalmatysiak02@gmail.com>
2018-05-20 14:59:10 +0200

    Add all files

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine',' pug');
+app.set('views','./views');
+
+app.get('/', function(req, res) {
+	res.render('log');
+});
+
+app.get('/auth/google', function(req, res) {
+	res.render('loggoogle', {
+		login: req.query.login,
+		password: req.query.password
+	});
+});
+
+app.listen(3000);
+app.use(function(req, res, next) {
+	res.status(404).send('Wybacz, nie mogliśmy odnależć tego, czego szukałeś!');
+});
