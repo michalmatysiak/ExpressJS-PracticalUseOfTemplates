0b37f45037f7366dcca40327b351408915a6c0c1
michalmatysiak <37140076+michalmatysiak@users.noreply.github.com>
2018-05-21 09:25:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('view engine',' pug');
+app.set('view engine','pug');
 app.set('views','./views');
 
 app.get('/', function(req, res) {
